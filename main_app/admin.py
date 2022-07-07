4fd6701fad6b3f7316d55be7f5645f5ac2ca4cd2
Nick Molina <104439965+nicholasmolina2019@users.noreply.github.com>
2022-07-07 08:25:38 -0500

    added form.py

--- main_app/admin.py
+++ main_app/admin.py
@@ -2,6 +2,8 @@
 from django.contrib import admin
 # import your models here
 from .models import Cat
+from .models import Feeding
 
 # Register your models here
 admin.site.register(Cat)
+admin.site.register(Feeding)
--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from django.urls import reverse
 # Register your models here.
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
 
 
 class Cat(models.Model):
@@ -9,16 +14,25 @@ class Cat(models.Model):
     description = models.TextField(max_length=250)
     age = models.IntegerField()
 
-
-    
-
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={"cat_id": self.id})
-    
-    class Feeding(models.Model):
-     date = models.DateField()
-     meal = models.CharField(max_field=1)
-    
+
+
+class Feeding(models.Model):
+
+
+    date = models.DateField('Feeding data')
+    meal = models.CharField(
+     max_length=1,
+     choices=MEALS,
+     default=MEALS[0][0]
+)
+#foreign key (fk)
+    cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return f"{self.get_meal_display()} on {self.date}"
